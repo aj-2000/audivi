16d6f49221c61a374421f6c0bf368b3be14c1b05
Ajay Sharma <hi@ajaysharma.dev>
2023-11-29 01:54:03 +0530

    add file input component

---
+++ src/components/FileInput.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { AppState } from '$lib';
+
+	let appState = AppState.instance;
+
+	function onFileChange(e: Event) {
+		const target = e.target as HTMLInputElement;
+		if (target.files) {
+			if (target.files.length > 0) {
+				appState.inputFile = target.files[0];
+				console.log(appState.inputFile);
+			}
+		}
+	}
+</script>
+
+<section>
+	<input
+		onchange={onFileChange}
+		type="file"
+		id="input-file"
+		name="input"
+		accept="audio/mp3,audio/*;capture=microphone"
+	/>
+</section>
